b491682c47f3922290cce5dde60787e4bba93d23
涛 <997933096@qq.com>
2019-04-05 21:56:38 +0800

    添加项目文件。

---
+++ WebAPI.xUnit/ValuesTests.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Newtonsoft.Json;
+using System.Net;
+using System.Net.Http;
+using System.Net.Mime;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace WebAPI.xUnit
+{
+    public class ValuesTests
+    {
+        public ValuesTests(ITestOutputHelper outputHelper)
+        {
+            var server = new TestServer(WebHost.CreateDefaultBuilder()
+                .UseStartup<Startup>());
+            Client = server.CreateClient();
+            Output = outputHelper;
+        }
+
+        public HttpClient Client { get; }
+        public ITestOutputHelper Output { get; }
+        [Fact]
+        public async Task GetById_ShouldBe_Ok()
+        {
+            // Arrange
+            var id = 1;
+
+            // Act
+            var response = await Client.GetAsync($"/api/values/{id}");
+
+            // Assert
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task Post_ShouldBe_OK()
+        {
+            var content = new StringContent(JsonConvert.SerializeObject(new { Name = "cxt", Age = 22 }), Encoding.UTF8, MediaTypeNames.Application.Json);
+
+            var response = await Client.PostAsync("/api/values", content);
+
+            // Output
+            var responseTest = await response.Content.ReadAsStringAsync();
+
+            Output.WriteLine(responseTest);
+
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+    }
+}
---
+++ WebAPI/Controllers/ValuesController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return $"value:{id}";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public ActionResult<string> Post(dynamic obj)
+        {
+            return $"姓名：{obj.Name},年龄：{obj.Age}";
+        }
+    }
+}
